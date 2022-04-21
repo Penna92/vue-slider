3b5c11130b464b96f8d7563ec1ff54ac1330196c
Penna92 <giacomopenna92@gmail.com>
2022-04-21 15:58:26 +0200

    Completato bonus 1 (cambio immagine centrale al click sulla miniatura corrispondente)

--- js/script.js
+++ js/script.js
@@ -6,64 +6,77 @@
 // 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 
 const app = new Vue({
-    el: "#app",
-    data: {
-         slides: [
-            {
-                image: 'img/01.jpg',
-                title: 'Svezia',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-            },
-            {
-                image: 'img/02.jpg',
-                title: 'Svizzera',
-                text: 'Lorem ipsum.',
-            },
-            {
-                image: 'img/03.jpg',
-                title: 'Gran Bretagna',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-            },
-            {
-                image: 'img/04.jpg',
-                title: 'Germania',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-            },
-            {
-                image: 'img/05.jpg',
-                title: 'Paradise',
-                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-            }
-        ],
-        activeIndex: 0,
-        intervalId: null,
+  el: "#app",
+  data: {
+    slides: [
+      {
+        image: "img/01.jpg",
+        title: "Svezia",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+      },
+      {
+        image: "img/02.jpg",
+        title: "Svizzera",
+        text: "Lorem ipsum.",
+      },
+      {
+        image: "img/03.jpg",
+        title: "Gran Bretagna",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        image: "img/04.jpg",
+        title: "Germania",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.",
+      },
+      {
+        image: "img/05.jpg",
+        title: "Paradise",
+        text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.",
+      },
+    ],
+    activeIndex: 0,
+    intervalId: null,
+  },
+  methods: {
+    nextSlide() {
+      if (this.activeIndex === 4) {
+        this.activeIndex = 0;
+      } else {
+        this.activeIndex += 1;
+      }
     },
-    methods: {
-        nextSlide(){
-            if(this.activeIndex === 4){
-                this.activeIndex = 0;
-            } else {
-                this.activeIndex += 1;
-            }
-        },
-        prevSlide(){
-            if(this.activeIndex === 0){
-                this.activeIndex = 4;
-            } else {
-                this.activeIndex -= 1;
-            }
-        },
-        autoScroll(){
-            this.intervalId = setInterval(()=>{
-                this.nextSlide();
-            }, 3000)
-        },
-        stopAutoScroll(){
-            clearInterval(this.intervalId);
-            this.intervalId = null;
+    prevSlide() {
+      if (this.activeIndex === 0) {
+        this.activeIndex = 4;
+      } else {
+        this.activeIndex -= 1;
+      }
+    },
+    autoScroll() {
+      this.intervalId = setInterval(() => {
+        this.nextSlide();
+        // console.log(this.activeIndex, this);
+      }, 3000);
+    },
+    stopAutoScroll() {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    },
+    changeOnClick() {
+      event.currentTarget.classList.add("active");
+      const thumbs = document.getElementsByClassName("thumb");
+      const imgs = document.getElementsByClassName("item");
+      for (let i = 0; i < thumbs.length; i++) {
+        if (event.currentTarget === thumbs[i]) {
+          this.activeIndex = i;
+          console.log(this.activeIndex);
+          //   imgs[i].classList.add("active-animation")
         }
+      }
     },
-    mounted(){
-        this.autoScroll();
-    }
-})
+  },
+  mounted() {
+    this.autoScroll();
+  },
+});
